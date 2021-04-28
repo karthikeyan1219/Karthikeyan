6d997185beee655911b9bf9e3b2b5db7c9d186b7
karthi1219 <karthikeyan2261998@gmail.com>
2021-04-27 22:18:25 -0700

    Success

---
+++ src/org/main/Reverse.java
@@ -0,0 +1,30 @@
+package org.main;
+
+public class Reverse {
+	
+	public static void main(String[] args) {
+		String s="madam";
+		String t="";
+		for(int i=s.length()-1;i>=0;i--) {
+		
+			t=t+s.charAt(i);
+		}
+		System.out.println(t);
+		
+		if(s==t)
+		{	
+		System.out.println("It is not a Palindrome");
+		}
+		else
+		{
+		System.out.println("It is  a Palindrome");	
+		}
+		}
+		
+	}
+	
+	
+	
+	
+
+
